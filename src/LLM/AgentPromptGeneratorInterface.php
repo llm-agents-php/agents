4bf750ad6f117d0e8e7d7f327b6cf0b01c6316b9
Pavel Buchnev <butschster@gmail.com>
2024-09-04 21:32:59 +0400

    Improves generator method arguments

--- src/LLM/AgentPromptGeneratorInterface.php
+++ src/LLM/AgentPromptGeneratorInterface.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace LLM\Agents\LLM;
 
 use LLM\Agents\Agent\AgentInterface;
+use LLM\Agents\LLM\Prompt\Chat\Prompt;
 use LLM\Agents\LLM\Prompt\Chat\PromptInterface;
 
 interface AgentPromptGeneratorInterface
 {
     public function generate(
         AgentInterface $agent,
-        string|\Stringable $prompt,
+        string|\Stringable $userPrompt,
         PromptContextInterface $context,
+        PromptInterface $prompt = new Prompt(),
     ): PromptInterface;
 }
