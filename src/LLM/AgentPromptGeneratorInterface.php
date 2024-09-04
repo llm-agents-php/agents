db2637284a5bf840cc74b34545dd697da69a80a5
Pavel Buchnev <butschster@gmail.com>
2024-09-04 20:35:48 +0400

    fix

--- src/LLM/AgentPromptGeneratorInterface.php
+++ src/LLM/AgentPromptGeneratorInterface.php
@@ -12,6 +12,6 @@ interface AgentPromptGeneratorInterface
     public function generate(
         AgentInterface $agent,
         string|\Stringable $prompt,
-        array|null|PromptContextInterface $context = null,
+        PromptContextInterface $context,
     ): PromptInterface;
 }
