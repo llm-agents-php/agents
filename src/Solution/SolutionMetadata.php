d1d5f0a07d3b9f7353b95b8a86f688607536b343
Pavel Buchnev <butschster@gmail.com>
2024-09-06 00:34:27 +0400

    Unfinal SolutionMetadata class

--- src/Solution/SolutionMetadata.php
+++ src/Solution/SolutionMetadata.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace LLM\Agents\Solution;
 
-final readonly class SolutionMetadata
+readonly class SolutionMetadata
 {
     public function __construct(
         public MetadataType $type,
