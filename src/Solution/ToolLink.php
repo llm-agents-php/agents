ce8dc7e825ce609d84559123f2f9f20f7ebc163c
Pavel Buchnev <butschster@gmail.com>
2024-09-06 00:35:36 +0400

    Adds description for tool link

--- src/Solution/ToolLink.php
+++ src/Solution/ToolLink.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace LLM\Agents\Solution;
 
+/**
+ * A tool link is a solution that points to a tool that can be used to solve a problem.
+ */
 class ToolLink extends Solution
 {
     public function __construct(
